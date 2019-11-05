ca85d85c16a722b2b0cc3dff768b87d3534f4fc2
grzesiek-hue <57141452+grzesiek-hue@users.noreply.github.com>
2019-11-05 22:48:33 +0100

    Update historyjka .py

--- historyjka .py
+++ historyjka .py
@@ -1,15 +1,15 @@
 from tkinter import *
 from tkinter import filedialog
-def wczytaj_plik():
+def open_file():
    result =  filedialog.askopenfile(initialdir="/", title="wybierz plik", filetypes=(("text files", ".txt"), ("all files", "*.*")))
    print(result)
    for c in result:
        print(c)
-def koniec() :
+def exit():
     sys.exit()
 root = Tk()
-przycisk = Button(root, text="wczytaj plik", command=wczytaj_plik)
-przycisk1= Button(root, text="koniec", command=koniec)
-przycisk.pack()
-przycisk1.pack()
+Button = Button(root, text="wczytaj plik", command=open_file)
+Button1= Button(root, text="koniec", command=exit)
+Button.pack()
+Button1.pack()
 root.mainloop()
