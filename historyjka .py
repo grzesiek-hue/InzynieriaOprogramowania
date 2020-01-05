f13e486688436e8815f265c55b8fd9bf7b1a7b1d
TraumenBM <56973042+TraumenBM@users.noreply.github.com>
2020-01-05 20:55:44 +0100

    Update historyjka .py

--- historyjka .py
+++ historyjka .py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import filedialog
+from funkcje import findFunctions
 calls = []
 calls_counter = 0
+functionsInFiles = {}
 def open_file():
     path = filedialog.askopenfilenames(initialdir="/", title="wybierz plik")
+    print(path)
     for lines in path:
         file_path = lines
         result = open(file_path)
@@ -24,26 +27,41 @@ def open_file():
                         if "from" in c:
                             position1 = c.find(i)
                             position2 = c.find("from")
-                            calls.append(
-                                [file_name, c[position2 + len("from "):position1]])  # dodanie nazwy pliku to tablicy
-                            calls_counter = calls_counter + 1
+                            for file in path:
+                                filePostion1 = file.index(".py")
+                                filePostion2 = file.rindex("/")
+                                fileName = file[filePostion2 + 1: filePostion1 + 3]
+                                if fileName == c[position2 + len("from "):position1 - 1] + ".py":
+                                    calls.append([file_name, c[position2 + len("from "):position1 - 1]])  # dodanie nazwy pliku to tablicy
+                                    calls_counter = calls_counter + 1
                         else:
                             position1 = c.find(i)
                             position2 = len(c)
-                            calls.append(
-                                [file_name, c[position1 + len(i):position2 - 1]])  # dodanie nazwy pliku to tablicy
-                            calls_counter = calls_counter + 1
+                            for file in path:
+                                filePostion1 = file.index(".py")
+                                filePostion2 = file.rindex("/")
+                                fileName = file[filePostion2 + 1: filePostion1 + 3]
+                                if fileName == c[position1 + len(i) + 1:position2 - 1] + ".py":
+                                    calls.append(
+                                        [file_name,
+                                         c[position1 + len(i) + 1:position2 - 1]])  # dodanie nazwy pliku to tablicy
+                                    calls_counter = calls_counter + 1
                     if i == "using" or i == "include" or i == "open" or "open(" in i:
                         # global calls_counter
                         position1 = c.find(i)
                         position2 = len(c)
-                        calls.append([file_name, c[position1 + len(i):position2 - 1]])  # dodanie nazwy pliku to tablicy
-                        calls_counter = calls_counter + 1
+                        for file in path:
+                            filePostion1 = file.index(".py")
+                            filePostion2 = file.rindex("/")
+                            fileName = file[filePostion2 + 1: filePostion1 + 3]
+                            if fileName == c[position1 + len(i) + 1:position2 - 1] + ".py":
+                                calls.append([file_name, c[position1 + len(i) + 1:position2 - 1]])  # dodanie nazwy pliku to tablicy
+                                calls_counter = calls_counter + 1
                     if "#" in (i):
                         break
-        for i in range(0, calls_counter):
-           # print("wywolanie numer ", i + 1, "zawiera: ", calls[i])      # dałem w komentarz bo troche zbugowane
-            pass
+        #for i in range(0, calls_counter):
+        #    print("wywolanie numer ", i + 1, "zawiera: ", calls[i])      # dałem w komentarz bo troche zbugowane
+        #    pass
 def exit() :
     sys.exit()
 def convert(calls):         #zamiana listy w tuple
@@ -52,11 +70,17 @@ def dep():              #wypisanie danych pod graf
     print(convert(calls))
 def Graph():
     data_to_graph=convert(calls)
+def func():
+    files = filedialog.askopenfilenames(initialdir="/", title="wybierz plik")
+    for file in files:
+        functionsInFiles[file] = findFunctions(file)
 root = Tk()
 button = Button(root, text="wczytaj plik", command=open_file)
 button1= Button(root, text="koniec", command=exit)
 button2= Button(root,text="pokaz zaleznosci",command=dep)
+button3= Button(root,text="szukaj funkcji",command=func)
 button.pack()
 button1.pack()
 button2.pack()
+button3.pack()
 root.mainloop()
